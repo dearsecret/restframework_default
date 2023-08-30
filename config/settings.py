26ca53c01538afc44d35885b63d9d0f948e67b3f
dearsecret <dearsecret1217@gmail.com>
2023-08-30 16:42:32 +0900

    jwt-login

--- config/settings.py
+++ config/settings.py
@@ -100,6 +100,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
+        "config.authentication.JWTauthetication",
     ]
 }
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
+import jwt
+from config import settings
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.exceptions import ParseError, AuthenticationFailed
+from rest_framework.response import Response
+from .serializers import PrivateUserSerializer
 
 # Create your views here.
+
+
+class JWTLogin(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            raise ParseError
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+
+        if user:
+            token = jwt.encode({"pk": user.pk}, settings.secret_key, algorithm="HS256")
+            return Response({"token": token})
+        else:
+            return Response({"error": "check password"})
